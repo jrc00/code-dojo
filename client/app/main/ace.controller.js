f7aa9610eee9b5b64bda764b383852b74b93c9be
Jeff Carter <jcarter00@gmail.com>
2016-03-03 12:21:46 -0500

    configure ace for multiple languages

--- client/app/main/ace.controller.js
+++ client/app/main/ace.controller.js
@@ -1,10 +1,126 @@
-angular.module('codeDojoApp')
-
 function AceCtrl($scope) {
-    // The modes
-  $scope.modes = ['Scheme', 'XML', 'Javascript'];
-  $scope.mode = $scope.modes[0];
 
+    // The modes
+  $scope.modes = ['javascript',
+    'abap',
+    'actionscript',
+    'ada',
+    'apache_conf',
+    'asciidoc',
+    'assembly_x86',
+    'autohotkey',
+    'batchfile',
+    'c9search',
+    'c_cpp',
+    'cirru',
+    'clojure',
+    'cobol',
+    'coffee',
+    'coldfusion',
+    'csharp',
+    'css',
+    'curly',
+    'd',
+    'dart',
+    'diff',
+    'dockerfile',
+    'dot',
+    'dummy',
+    'dummysyntax',
+    'eiffel',
+    'ejs',
+    'elixir',
+    'elm',
+    'erlang',
+    'forth',
+    'ftl',
+    'gcode',
+    'gherkin',
+    'gitignore',
+    'glsl',
+    'golang',
+    'groovy',
+    'haml',
+    'handlebars',
+    'haskell',
+    'haxe',
+    'html',
+    'html_ruby',
+    'ini',
+    'io',
+    'jack',
+    'jade',
+    'java',
+    'json',
+    'jsoniq',
+    'jsp',
+    'jsx',
+    'julia',
+    'latex',
+    'less',
+    'liquid',
+    'lisp',
+    'livescript',
+    'logiql',
+    'lsl',
+    'lua',
+    'luapage',
+    'lucene',
+    'makefile',
+    'markdown',
+    'matlab',
+    'mel',
+    'mushcode',
+    'mysql',
+    'nix',
+    'objectivec',
+    'ocaml',
+    'pascal',
+    'perl',
+    'pgsql',
+    'php',
+    'powershell',
+    'praat',
+    'prolog',
+    'properties',
+    'protobuf',
+    'python',
+    'r',
+    'rdoc',
+    'rhtml',
+    'ruby',
+    'rust',
+    'sass',
+    'scad',
+    'scala',
+    'scheme',
+    'scss',
+    'sh',
+    'sjs',
+    'smarty',
+    'snippets',
+    'soy_template',
+    'space',
+    'sql',
+    'stylus',
+    'svg',
+    'tcl',
+    'tex',
+    'text',
+    'textile',
+    'toml',
+    'twig',
+    'typescript',
+    'vala',
+    'vbscript',
+    'velocity',
+    'verilog',
+    'vhdl',
+    'xml',
+    'xquery',
+    'yaml'];
+
+  $scope.mode = $scope.modes[0];
 
   // The ui-ace option
   $scope.aceOption = {
@@ -15,16 +131,13 @@ function AceCtrl($scope) {
       $scope.modeChanged = function () {
         _ace.getSession().setMode("ace/mode/" + $scope.mode.toLowerCase());
       };
-
     }
   };
 
   // Initial code content...
-  $scope.aceModel = ';; Scheme code in here.\n' +
-    '(define (double x)\n\t(* x x))\n\n\n' +
-    '<!-- XML code in here. -->\n' +
-    '<root>\n\t<foo>\n\t</foo>\n\t<bar/>\n</root>\n\n\n' +
-    '// Javascript code in here.\n' +
-    'function foo(msg) {\n\tvar r = Math.random();\n\treturn "" + r + " : " + msg;\n}';
+  $scope.aceModel = '// Code goes here...\n' +
+  'function foo(msg) {\n\tvar r = Math.random();\n\treturn "" + r + " : " + msg;\n}';
+}
 
-  }
+angular.module('codeDojoApp')
+.controller('AceCtrl', AceCtrl);
